95fcf28f027b1cb1e9f5901ecef89478db0aa150
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-03-12 15:09:25 +0530

    Commit

---
+++ DefaultParameter/script.js
@@ -0,0 +1,24 @@
+// function countTo5(count=true){
+//     if(count===true){
+//         for(let i=1;i<=5;i++){
+//             console.log(`Count : ${i}`);
+//         }
+//     }
+// }
+// countTo5();
+
+// function ratings(rate=5){
+//     if(rate === 5){
+//         console.log(`High Ratings :)`);
+//     }
+//     else if(rate === 0){
+//         console.log(`Low Ratings :(`);
+//     }
+// }
+// ratings(0);
+
+
+function multiply(a,b=1){
+    console.log(a*b);
+}
+multiply(2);
