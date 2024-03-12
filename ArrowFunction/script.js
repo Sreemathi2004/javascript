a513f04742ab106a1cb41b074f2deff0f8f72298
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-03-12 15:07:09 +0530

    Commit

---
+++ ArrowFunction/script.js
@@ -0,0 +1,18 @@
+//  greet=username=> `Hello , ${username}`;
+ 
+// console.log(greet('Sree'));
+
+
+//  double=number=>number*2;
+
+// console.log(double(20));
+
+
+setTimeout(()=>console.log("Hello"),
+    setTimeout(()=>console.log('Hey'),
+    setTimeout(()=>console.log("Namaste"),
+    setTimeout(()=>console.log("Hi"),
+    setTimeout(()=>console.log("BonJour"),2000),2000),2000),2000),2000);
+    
+
+
