fa7624e8f28fc36042f6ecf57dc78e849f3b5dc9
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-03-14 10:49:13 +0530

    updated

--- DOM_14Project/script.js
+++ DOM_14Project/script.js
@@ -26,19 +26,39 @@ var booktitleinput=document.getElementById("book-title-input");
 var bookauthorinput=document.getElementById("book-author-input");
 var bookdescriptioninput=document.getElementById("book-description-input");
 addbook.addEventListener("click",function(event){
-    event.preventDefault()
+    event.preventDefault();
     var div=document.createElement("div");
     div.setAttribute("class","book-container");
+    
     div.innerHTML=` <h2>${booktitleinput.value}</h2>
     <h5>${bookauthorinput.value}</h5>
     <p>${bookdescriptioninput.value}</p>
     <button onclick="deleteContent(event)">Delete</button>`
+
+    if (booktitleinput.value === "") {
+        alert(`Kindly Enter Your "BookTitle"`);
+        return; // Exit the function
+    }
+    else if (bookauthorinput.value === "") {
+        alert(`Kindly Enter Your "BookAuthor"`);
+        return; // Exit the function
+    }
+   else if (bookdescriptioninput.value === "") {
+        alert(`Kindly Enter Your "BookDescription"`);
+        return; // Exit the function
+    }
     container.append(div);
+    booktitleinput.value = "";
+    bookauthorinput.value = "";
+    bookdescriptioninput.value = "";
+   
     popupoverlay.style.display="none";
     popupbox.style.display="none";
+    
 })
 
 function deleteContent(event){
     event.target.parentElement.remove();
 }
+
 
